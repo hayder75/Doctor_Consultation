b6df9933c62f7e687ab0f3af60e3cb0aa6893d7e
hayder75 <78726242+hayder75@users.noreply.github.com>
2024-05-07 23:39:56 +0300

    New model

---
+++ models/singlemodel.js
@@ -0,0 +1,19 @@
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isDoctor: {
+      type: Boolean,
+      default: false,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    seenNotifications: {
+      type: Array,
+      default: [],
+    },
